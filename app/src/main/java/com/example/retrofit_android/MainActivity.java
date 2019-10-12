b356b78b18bd310843571cecbd62c86adc1baf04
shrinidhi99 <shrinidhi99.varna@gmail.com>
2019-10-13 01:19:49 +0530

    put patch and delete

--- app/src/main/java/com/example/retrofit_android/MainActivity.java
+++ app/src/main/java/com/example/retrofit_android/MainActivity.java
@@ -36,7 +36,9 @@ public class MainActivity extends AppCompatActivity {
 
 //        getPosts();
 //        getComments();
-        createPost();
+//        createPost();
+//        updatePost();
+        deletePost();
     }
 
     private void getPosts() {
@@ -102,6 +104,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
     private void createPost() {
         Post post = new Post(23, "New Title", "New Text");
 
@@ -134,6 +137,52 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onFailure(Call<Post> call, Throwable t) {
+                textViewResult.setText(t.getMessage());
+            }
+        });
+    }
+
+    private void updatePost() {
+        Post post = new Post(12, null, "New Text");
+        Call<Post> call = jsonPlaceHolderApi.patchPost(5, post);
+        call.enqueue(new Callback<Post>() {
+            @Override
+            public void onResponse(Call<Post> call, Response<Post> response) {
+                if (!response.isSuccessful()) {
+                    textViewResult.setText("Code: " + response.code());
+                    return;
+                }
+
+                Post postResponse = response.body();
+
+                String content = "";
+                content += "Code: " + response.code() + "\n";
+                content += "ID: " + postResponse.getId() + "\n";
+                content += "User ID: " + postResponse.getUserId() + "\n";
+                content += "Title: " + postResponse.getTitle() + "\n";
+                content += "Text: " + postResponse.getText() + "\n\n";
+
+                textViewResult.setText(content);
+            }
+
+            @Override
+            public void onFailure(Call<Post> call, Throwable t) {
+                textViewResult.setText(t.getMessage());
+            }
+        });
+    }
+
+    private void deletePost() {
+        Call<Void> call = jsonPlaceHolderApi.deletePost(5);
+
+        call.enqueue(new Callback<Void>() {
+            @Override
+            public void onResponse(Call<Void> call, Response<Void> response) {
+                textViewResult.setText("Code: " + response.code());
+            }
+
+            @Override
+            public void onFailure(Call<Void> call, Throwable t) {
                 textViewResult.setText(t.getMessage());
             }
         });
