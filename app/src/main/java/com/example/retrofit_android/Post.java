509b64eec15105fb569cf7226c815f40f5e47716
shrinidhi99 <shrinidhi99.varna@gmail.com>
2019-10-13 00:29:42 +0530

    post request and form urlencoded

--- app/src/main/java/com/example/retrofit_android/Post.java
+++ app/src/main/java/com/example/retrofit_android/Post.java
@@ -4,11 +4,17 @@ import com.google.gson.annotations.SerializedName;
 
 public class Post {
     private int userId;
-    private int id;
+    private Integer id;
     private String title;
 
     @SerializedName("body")
     private String text;
+
+    public Post(int userId, String title, String text) {
+        this.userId = userId;
+        this.title = title;
+        this.text = text;
+    }
 
     public int getUserId() {
         return userId;
